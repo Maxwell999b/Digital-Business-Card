9d856bbb418ccc8f4b6349abebe5410ecb313f20
Abdulrahman Ahmed <bedo150@gmail.com>
2024-07-09 19:28:17 +0300

    fixed images url/path for vercel ,moved to public folder: update 1 file and delete 8 files

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => (
     </h3>
     <div className="projects--grid">
       <a href="https://anime-cards-anime-gogo-tv.vercel.app/" target="_blank" className="projects--card">
-        <img src="../src/assets/1.png" alt="Project 1" className="projects--image" />
-        <img src="../src/assets/1-2.png" alt="Project 1 Hover" className="projects--image-hover" />
+        <img src="/assets/1.png" alt="Project 1" className="projects--image" />
+        <img src="/assets/1-2.png" alt="Project 1 Hover" className="projects--image-hover" />
         <div className="projects--overlay">
           <p className="projects--text">
             <a
@@ -22,8 +22,8 @@ const Projects = () => (
         </div>
       </a>
       <a href="https://pixel-store-seven.vercel.app/" target="_blank" className="projects--card">
-        <img src="../src/assets/2.png" alt="Project 2" className="projects--image" />
-        <img src="../src/assets/2-2.png" alt="Project 2 Hover" className="projects--image-hover" />
+        <img src="/assets/2.png" alt="Project 2" className="projects--image" />
+        <img src="/assets/2-2.png" alt="Project 2 Hover" className="projects--image-hover" />
         <div className="projects--overlay">
           <p className="projects--text">
             <a href="https://github.com/Maxwell999b/pixel-store" target="_blank" className="projects--card">
@@ -34,8 +34,8 @@ const Projects = () => (
       </a>
 
       <a href="https://maxwell999b.github.io/Front-End-Portfolio/" target="_blank" className="projects--card">
-        <img src="../src/assets/4.png" alt="Project 4" className="projects--image" />
-        <img src="../src/assets/4-2.png" alt="Project 4 Hover" className="projects--image-hover" />
+        <img src="/assets/4.png" alt="Project 4" className="projects--image" />
+        <img src="/assets/4-2.png" alt="Project 4 Hover" className="projects--image-hover" />
         <div className="projects--overlay">
           <p className="projects--text">
             <a href="https://github.com/Maxwell999b/Front-End-Portfolio" target="_blank" className="projects--card">
@@ -48,8 +48,8 @@ const Projects = () => (
         href="https://addons.mozilla.org/en-US/firefox/addon/midnight-radiance-theme/"
         target="_blank"
         className="projects--card">
-        <img src="../src/assets/3.png" alt="Project 3" className="projects--image" />
-        <img src="../src/assets/3-2.png" alt="Project 3 Hover" className="projects--image-hover" />
+        <img src="/assets/3.png" alt="Project 3" className="projects--image" />
+        <img src="/assets/3-2.png" alt="Project 3 Hover" className="projects--image-hover" />
         <div className="projects--overlay">
           <p className="projects--text">
             <a href="https://github.com/Maxwell999b/Midnight-Radiance-Theme" target="_blank" className="projects--card">
